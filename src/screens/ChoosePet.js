206ad40a453a9235b70cb7cd69295cf1961dbf14
elianacohen-tu <tug37175@temple.edu>
2021-03-08 22:03:01 -0500

    Added in text input, still have issues with formatting

--- src/screens/ChoosePet.js
+++ src/screens/ChoosePet.js
@@ -1,46 +1,60 @@
 import React from 'react';
-import {Text, View, Button, StyleSheet, Image} from "react-native";
+import {Text, View, Button, StyleSheet, Image, TextInput} from "react-native";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
         backgroundColor: '#406BE9',
     },
     text: {
-        color: 'white'
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
     },
+    text2: {
+            color: 'black',
+            fontSize: 20,
+            textAlign: 'center',
+            fontWeight: 'bold',
+        },
     image: {
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
     },
-});
+    imageBoxes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    },
+    }
+);
 
 const choosePetScreen = () => {
+    const [value, onChangeText] = React.useState('');
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Please choose your start pet</Text>
-            <View style={{
-                          flex: 1,
-                          flexDirection: 'row',
-                          justifyContent: 'space-between',
-                          }}>
-            <Image
-                style={styles.image}
-                source={require('../images/cutekitten.png')}
-            />
-            <Image
-                style={styles.image}
-                source={require('../images/lizard.png')}
-            />
-            <Image
-                style={styles.image}
-                source={require('../images/pixeldog.png')}
-            />
-            </View>
-        </View>
 
+        <View style={styles.container}>
+            <Text style={styles.text}>Please choose your starter pet</Text>
+            <View style={styles.imageBoxes}>
+                <Image
+                 style={styles.image}
+                 source={require('../images/cutekitten.png')}
+                />
+                <Image
+                    style={styles.image}
+                    source={require('../images/lizard.png')}
+                />
+                <Image
+                    style={styles.image}
+                   source={require('../images/pixeldog.png')}
+                 />
+            </View>
+            <Text style={styles.text}>Please enter a name for your pet</Text>
+            <TextInput
+                  style={styles.text2}
+                  onChangeText={text => onChangeText(text)}
+                  value={value}
+                />
+        </View>
     );
 };
 
