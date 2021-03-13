59921f214216e14afc5c0ff38d519f57e0f6d481
Erin McDonald <tug94529@temple.edu>
2021-03-13 18:12:39 -0500

    very basic database connection

--- src/databases/Task.js
+++ src/databases/Task.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import {Text, View, Button, StyleSheet} from "react-native";
-import * as SQLite from 'expo-sqlite'
+import * as  SQLite from 'expo-sqlite'
 import { BaseModel, types } from 'expo-sqlite-orm'
- 
-export default class Tasks extends BaseModel {
+
+export default class Task extends BaseModel {
   constructor(obj) {
     super(obj)
   }
- 
+
   static get database() {
     return async () => SQLite.openDatabase('tasks.db')
   }
- 
+
   static get tableName() {
     return 'tasks'
   }
- 
+
   static get columnMapping() {
     return {
+      id: { type: types.INTEGER, primary_key: true },
       name: { type: types.TEXT, not_null: true },
-      dueTime: { type: types.INTEGER, default: () => Date.now() }
-      complete: { type: types.BOOLEAN},
+      due: { type: types.NUMERIC },
+      timestamp: { type: types.INTEGER, default: () => Date.now() }
     }
   }
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet} from "react-native";
+import React, { useCallback, useState } from 'react';
+import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native'
+import Task from '../databases/Task'
+
+export default function Home() {
+ Task.dropTable()
+ const [tasks, setTasks] = useState([])
+  const createTable = useCallback(async () => {
+    await Task.createTable()
+    Alert.alert('Table created successfully')
+  }, [])
+
+  const createTask = useCallback(async () => {
+    const props = {
+      name: 'hi',
+      due: 80
+    }
+
+    const task = new Task(props)
+    await task.save()
+    setTasks(await Task.query())
+  }, [])
+
+  if (Platform.OS === 'web') {
+    return <View style={styles.container}>
+      <Text>Not supported in web platform</Text>
+    </View>
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={{ padding: 20 }} onPress={createTable}>
+        <Text>Create table</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{ padding: 20 }} onPress={createTask}>
+        <Text>Create Task</Text>
+      </TouchableOpacity>
+      <ScrollView style={{ flex: 1 }}>
+        {
+          tasks.map(task => <Text key={task.id}>{JSON.stringify(task)}</Text>)
+        }
+      </ScrollView>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
 	container: {
@@ -28,4 +71,3 @@ const HomeScreen = () => {
 	);
 };
 
-export default HomeScreen;
