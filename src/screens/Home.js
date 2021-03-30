b7063b8bc285c3e4da603f95ef9d192a4f97d7a9
Samir Singh <tuh03649@temple.edu>
2021-03-30 11:09:26 -0400

    extra task work

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,96 +1,131 @@
 import React, { useCallback, useState } from 'react';
 import { Image, Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View, Platform, TextInput, Button } from 'react-native'
 import Task from '../databases/Task';
-import {MaterialCommunityIcons} from "@expo/vector-icons";
-import { SQLite } from 'expo-sqlite';
-import DatabaseLayer from 'expo-sqlite-orm/src/DatabaseLayer';
-import { withSafeAreaInsets } from 'react-native-safe-area-context';
+import {MaterialCommunityIcons, Ionicons} from "@expo/vector-icons";
+import {useDispatch} from "react-redux";
+import {REWARD} from "../redux/coinBalance";
+import {showMessage} from "react-native-flash-message";
+import {ItemInventory} from "../components/ItemInventory";
 
-export default function Home() {
+//test
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#406be9',
 
- const [tasks, setTasks] = useState([])
+	},
 
-   Task.createTable()
+	listContainer:{
+		backgroundColor: 'rgba(49,69,194,0.7)',
+		shadowOffset: {width: 0, height: 5},
+		shadowOpacity: 0.8,
+		shadowRadius: 3,
+		borderWidth: 3,
+		borderColor: 'black',
+		width: '75%',
+		height: '80%',
+		marginTop: '2%',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginBottom: '5%',
+	},
+	listItem:{
+		paddingTop: '5%',
+		paddingBottom: '5%',
+		paddingLeft: '5%',
+		margin: '3%',
+		borderRadius: 10,
+		backgroundColor: 'white',
+		alignContent: 'center',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+	},
+	listFooter:{
+		borderTopColor: '#FFFFFF',
+		paddingLeft: '2%',
+		paddingTop: '5%',
+	},
+	text: {
+		color: 'white',
+	},
+	weather: {
+		color: 'white',
+		fontSize: 30,
+	}
+});
 
- const deleteTask = useCallback(async (deleteText) => {
-   const t = await Task.findBy({ name_eq: deleteText })
-	t.destroy()
-	await t.save()
-    setTasks(await Task.query())
-  }, [])
+const Home = () => {
+	const dispatch = useDispatch();
+
+	const [tasks, setTasks] = useState([])
+
+	const onSubmit = (task) => {
+		setTasks([...tasks, task]);
+		setText('');
+	}
+
+	const onDelete = (task) => {
+		setTasks([...tasks].filter(item => item !== task));
+	}
+
+	const onComplete = (task) => {
+		dispatch({type: REWARD, data: 5})
+		showMessage({
+			message: '5 coins have been added to your coin balance',
+			type: "success",
+			statusBarHeight: 52,
+		})
+		setTasks([...tasks].filter(item => item !== task));
+	}
 
 
-  const createTask = useCallback(async (text) => {
-    const props = {
-      name: text,
-      due: 80
-    }
+ 	const ListItem = ({taskName}) => {
+	  	return(
+			<View style={styles.listItem}>
+		  		<Text style={{fontWeight: 'bold', fontSize: 16, maxWidth:'77%'}} color='white'>{taskName}</Text>
+		  		<View style = {{flexDirection:'row'}}>
+					<TouchableOpacity
+						onPress={() => onDelete(taskName)}
+					>
+						<MaterialCommunityIcons
+							name = "trash-can-outline"
+							size = {24}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={() => onComplete(taskName)}
+					>
+						<Ionicons
+							name="checkmark"
+							size={24}
+						/>
+					</TouchableOpacity>
+				</View>
+			</View>
+	  	);
+	}
 
-    const task = new Task(props)
-    await task.save()
-    setTasks(await Task.query())
-  }, [])
-
-
-  const updateTask = useCallback(async (editText) => {
-
-  }, [])
-
- const onSubmit = () => {
- 	createTask(text);
-    //awardCoins();
- }
-
- const onDelete = () => {
- 	deleteTask(deleteText);
- }
-
- const onUpdate = () =>{
-   updateTask(editText);
- }
-
- const ListItem = (props) => {
-  const jsonObj = JSON.parse(props.value);
-
-  return(
-    <View style={styles.listItem}>
-      <Text style={{fontWeight: 'bold', fontSize: 16, maxWidth:'77%'}} color='white'>{jsonObj.name}</Text>
-
-      <View style = {{flexDirection:'row'}}>
-      <TouchableOpacity onPress={onUpdate}>
-        <MaterialCommunityIcons
-        name = "pencil"
-        size = {30}
-        style={{color:"#637ed0"}}/>
-        </TouchableOpacity>
-        </View>
-    </View>
-  );
- }
-
- const ListOfTasks = () => {
-   return tasks.map(task => {
-     return(
-       <ListItem key = {task.id}
-       updateTask = {()=> updateTask(task.id)}
-       value={JSON.stringify(task)} />
-
-       )
-   })
- }
+	const ListOfTasks = () => {
+	    return tasks.map((task, index) => {
+		 	return(
+		   		<ListItem key = {index}
+		   			taskName={task}
+				/>
+		   )
+	    })
+	}
 
 
 
 
    const [text, setText] = React.useState('');
    const [deleteText, setDeleteText] = React.useState('');
-   const[editText, setEditText]= React.useState({id: null, value: ''});
   return (
     <View style={styles.container}>
       <TextInput style={{marginTop: '2%', backgroundColor: 'white', color: 'black', width: '50%'}} label='name' value={text} onChangeText={text => setText(text)}/>
-      <Button title='submit'  color="#637ed0" onPress={onSubmit} />
-      <TextInput style={{backgroundColor: 'white', width: '50%'}} label='Delete' value={deleteText} onChangeText={deleteText => setDeleteText(deleteText)}/>
-	    <Button title="Delete" color="#637ed0" onPress={onDelete}/>
+      <Button title='submit'  color="#637ed0" onPress={() => onSubmit(text)} />
       <View style={styles.listContainer}>
         <ScrollView>
 
@@ -109,59 +144,6 @@ export default function Home() {
     </View>
   );
 }
-//test
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#406be9',
 
-	},
 
-  listContainer:{
-    backgroundColor: 'rgba(49,69,194,0.7)',
-    shadowOffset: {width: 0, height: 5},
-		shadowOpacity: 0.8,
-		shadowRadius: 3,
-    borderWidth: 3,
-    borderColor: 'black',
-    width: '75%',
-    height: '80%',
-    marginTop: '2%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: '5%',
-  },
-  listItem:{
-    paddingTop: '5%',
-    paddingBottom: '5%',
-    paddingLeft: '5%',
-    margin: '3%',
-    borderRadius: 10,
-    backgroundColor: 'white',
-    alignContent: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
-  listFooter:{
-    borderTopColor: '#FFFFFF',
-    paddingLeft: '2%',
-    paddingTop: '5%',
-  },
-	text: {
-		color: 'white',
-  	},
-	weather: {
-		color: 'white',
-		fontSize: 30,
-	}
-});
-
-const HomeScreen = () => {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Home Screen</Text>
-		</View>
-	);
-};
+export default Home;
