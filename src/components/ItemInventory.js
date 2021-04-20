1a470654a74292cd756214a66b7d657f8ec27966
Samir Singh <tuh03649@temple.edu>
2021-04-20 12:22:10 -0400

    added new items to the marketplace

--- src/components/ItemInventory.js
+++ src/components/ItemInventory.js
@@ -11,18 +11,6 @@ const ItemInventory = {
 		},
 		pointValue: 3
 	},
-	shoes: {
-		name: 'shoes',
-		uri: require('../test_images/shoes.png'),
-		cost: 10,
-		category: 'clothes',
-		buyText: 'Would you like to buy shoes?',
-		inventoryText: 'Would you like your pet to wear these shoes?',
-		wearUri: require('../images/red_shoes.png'),
-		benefits: {
-			Health: 0.2
-		}
-	},
 	pizza: {
 		name: 'pizza',
 		uri: require('../test_images/pizza.png'),
@@ -45,13 +33,74 @@ const ItemInventory = {
 			Hunger: 0.2
 		}
 	},
-	ball: {
-		name: 'ball',
-		uri: require('../test_images/ball.png'),
-		cost: 8,
-		category: 'toys',
-		buyText: 'Would you like to buy a ball?',
-		inventoryText: 'Would you like your pet to play with this ball?',
+	water: {
+		name: 'water',
+		uri: require('../test_images/water.png'),
+		cost: 2,
+		category: 'food',
+		buyText: 'Would you like to buy water?',
+		inventoryText: 'Would you like to give your pet a drink?',
+		benefits: {
+			Hunger: 0.2
+		}
+	},
+	lemonade: {
+		name: 'lemonade',
+		uri: require('../test_images/lemonade.png'),
+		cost: 4,
+		category: 'food',
+		buyText: 'Would you like to buy lemonade?',
+		inventoryText: 'Would you like your pet to drink this lemonade?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Hunger: 0.3
+		}
+	},
+	hot_chocolate: {
+		name: 'hot chocolate',
+		uri: require('../test_images/hot_chocolate.png'),
+		cost: 4,
+		category: 'food',
+		buyText: 'Would you like to buy hot chocolate?',
+		inventoryText: 'Would you like your pet to drink this hot chocolate?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Hunger: 0.3
+		}
+	},
+
+	shoes: {
+		name: 'shoes',
+		uri: require('../test_images/shoes.png'),
+		cost: 10,
+		category: 'clothes',
+		buyText: 'Would you like to buy shoes?',
+		inventoryText: 'Would you like your pet to wear these shoes?',
+		wearUri: require('../images/red_shoes.png'),
+		benefits: {
+			Health: 0.2
+		}
+	},
+	blue_shoes: {
+		name: 'blue shoes',
+		uri: require('../test_images/blue_shoes.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy blue shoes?',
+		inventoryText: 'Would you like your pet to wear these blue shoes?',
+		wearUri: require('../images/blue_shoes.png'),
+		benefits: {
+			Health: 0.2
+		}
+	},
+	black_shoes: {
+		name: 'black shoes',
+		uri: require('../test_images/black_shoes.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy black shoes?',
+		inventoryText: 'Would you like your pet to wear these black shoes?',
+		wearUri: require('../images/black_shoes.png'),
 		benefits: {
 			Fun: 0.3
 		}
@@ -66,29 +115,6 @@ const ItemInventory = {
 		wearUri: require('../images/white_shirt.png'),
 		benefits: {
 			Health: 0.3
-		}
-	},
-	water: {
-		name: 'water',
-		uri: require('../test_images/water.png'),
-		cost: 2,
-		category: 'food',
-		buyText: 'Would you like to buy water?',
-		inventoryText: 'Would you like to give your pet a drink?',
-		benefits: {
-			Hunger: 0.2
-		}
-	},
-	blue_shoes: {
-		name: 'blue shoes',
-		uri: require('../test_images/blue_shoes.png'),
-		cost: 12,
-		category: 'clothes',
-		buyText: 'Would you like to buy blue shoes?',
-		inventoryText: 'Would you like your pet to wear these blue shoes?',
-		wearUri: require('../images/blue_shoes.png'),
-		benefits: {
-			Health: 0.2
 		}
 	},
 	black_shirt: {
@@ -115,6 +141,101 @@ const ItemInventory = {
 			Health: 0.3
 		}
 	},
+	black_coat: {
+		name: 'black coat',
+		uri: require('../test_images/black_coat.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a black coat?',
+		inventoryText: 'Would you like your pet to wear this black coat?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	pink_coat: {
+		name: 'pink coat',
+		uri: require('../test_images/pink_coat.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a pink coat?',
+		inventoryText: 'Would you like your pet to wear this pink coat?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	blue_coat: {
+		name: 'blue coat',
+		uri: require('../test_images/blue_coat.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a blue coat?',
+		inventoryText: 'Would you like your pet to wear this blue coat?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	white_tank_top: {
+		name: 'white tank top',
+		uri: require('../test_images/tank_top.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a white tank top?',
+		inventoryText: 'Would you like your pet to wear this white tank top?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	blue_tank_top: {
+		name: 'blue tank top',
+		uri: require('../test_images/blue_tank_top.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a blue tank top?',
+		inventoryText: 'Would you like your pet to wear this blue tank top?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	green_tank_top: {
+		name: 'green tank top',
+		uri: require('../test_images/green_tank_top.png'),
+		cost: 12,
+		category: 'clothes',
+		buyText: 'Would you like to buy a green tank top?',
+		inventoryText: 'Would you like your pet to wear this green tank top?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	umbrella: {
+		name: 'umbrella',
+		uri: require('../test_images/umbrella.png'),
+		cost: 8,
+		category: 'clothes',
+		buyText: 'Would you like to buy an umbrella?',
+		inventoryText: 'Would you like your pet to use this umbrella?',
+		wearUri: require('../images/black_shoes.png'),
+		benefits: {
+			Health: 0.3
+		}
+	},
+	ball: {
+		name: 'ball',
+		uri: require('../test_images/ball.png'),
+		cost: 8,
+		category: 'toys',
+		buyText: 'Would you like to buy a ball?',
+		inventoryText: 'Would you like your pet to play with this ball?',
+		benefits: {
+			Fun: 0.3
+		}
+	},
 	red_ball: {
 		name: 'red ball',
 		uri: require('../test_images/red_ball.png'),
@@ -137,21 +258,9 @@ const ItemInventory = {
 			Fun: 0.3
 		}
 	},
-	black_shoes: {
-		name: 'black shoes',
-		uri: require('../test_images/black_shoes.png'),
-		cost: 12,
-		category: 'clothes',
-		buyText: 'Would you like to buy black shoes?',
-		inventoryText: 'Would you like your pet to wear these black shoes?',
-		wearUri: require('../images/black_shoes.png'),
-		benefits: {
-			Fun: 0.3
-		}
-	},
 	shampoo: {
 		name: 'shampoo',
-		uri: require('../test_images/carrot.png'),
+		uri: require('../test_images/shampoo.png'),
 		cost: 4,
 		category: 'grooming',
 		buyText: 'Would you like to buy shampoo?',
@@ -162,7 +271,7 @@ const ItemInventory = {
 	},
 	soap: {
 		name: 'soap',
-		uri: require('../test_images/carrot.png'),
+		uri: require('../test_images/soap.png'),
 		cost: 4,
 		category: 'grooming',
 		buyText: 'Would you like to buy soap?',
